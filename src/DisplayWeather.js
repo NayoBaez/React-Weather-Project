659c812e928d05351183cc6676b939f1d5170ee6
NayoBaez <104393006+NayoBaez@users.noreply.github.com>
2022-07-29 18:33:37 -0400

    Refactored DisplayWeather & Search Engine components
    Refactored and Restructured the Api call by moving it from DisplayWeather.js into the SearchEngine.js

---
+++ src/DisplayWeather.js
@@ -0,0 +1,36 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+import "./DisplayWeather.css";
+
+export default function DisplayWeather(props) {
+  return (
+    <div>
+      <div container="container-display-weather">
+        <div className="row weather-information">
+          <div className="col-3">
+            {" "}
+            <h1>{props.data.city}</h1>
+            <FormattedDate />
+            <p className="weather-description text-capitalize">
+              {props.data.description}
+            </p>
+          </div>
+          <div className="col-3">
+            <img src={props.data.icon} alt={props.data.description} />
+          </div>
+          <div className="col-3">
+            <h2>{Math.round(props.data.temperature)}°C</h2>
+          </div>
+          <div className="col-3">
+            {" "}
+            <p>
+              Humidity: {props.data.humidity}% Wind:{" "}
+              {Math.round(props.data.wind)} km/h
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,33 +1,62 @@
 import React, { useState } from "react";
-import DisplayCity from "./DisplayCity";
+import axios from "axios";
+import DisplayWeather from "./DisplayWeather";
 
 import "./SearchEngine.css";
 
 export default function SearchEngine() {
-  let [city, setCity] = useState("");
-  let [weather, setWeather] = useState(null);
+  let [city, setCity] = useState("Tokyo");
+  let [weatherData, setWeatherData] = useState({ ready: false });
+
+  function GetWeatherData(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      city: response.data.name,
+    });
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-
-    setWeather(city);
+    searchCity();
   }
 
   function showCity(event) {
     setCity(event.target.value);
   }
 
-  return (
-    <div className="SearchEngine">
-      <form className="search" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Enter Your City" onChange={showCity} />
-        <input type="submit" value="Search" />
-        <input type="submit" value="Current-location" />
-      </form>
-      <div className="container-center">
-        <div className="container-display-city">
-          <DisplayCity city={weather} />
+  function searchCity() {
+    let apiKey = "21d3636317e4ed6c448afdbbc3833fb2";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(GetWeatherData);
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="SearchEngine">
+        <form className="search" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter Your City"
+            onChange={showCity}
+          />
+          <input type="submit" value="Search" />
+          <input type="submit" value="Current-location" />
+        </form>
+        <div className="container-center">
+          <div className="container-display-city">
+            <DisplayWeather data={weatherData} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    searchCity();
+    return <h1>Loading</h1>;
+  }
 }
