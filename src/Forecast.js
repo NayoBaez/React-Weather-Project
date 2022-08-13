efb82d537fa83a96d71d18428957def3c0e6fb37
NayoBaez <104393006+NayoBaez@users.noreply.github.com>
2022-08-13 13:58:10 -0400

    Updates Forecast.js
    Added Return on forecast mapping for successful deployment

--- src/Forecast.js
+++ src/Forecast.js
@@ -27,6 +27,8 @@ export default function Forecast(props) {
                   <ForecastDay data={dailyforecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
