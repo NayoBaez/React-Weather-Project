7f8cab8f49277ac0da1952097f44da478fc024c3
NayoBaez <104393006+NayoBaez@users.noreply.github.com>
2022-09-16 16:21:13 -0400

    Fixed warnings and Mapbox error
    * Fixed the error from DisplayTemperature, had removed Fahrenheit in previous commit, but did not remove the use state.
    
    * Fixed errors from Mapbox, the Api is still not working properly at this point. But I will continue to fix it.

--- src/Maps.js
+++ src/Maps.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ReactMapGL from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export default function Maps(props) {
   const [viewport, setViewport] = useState({
@@ -11,11 +12,13 @@ export default function Maps(props) {
     <section>
       <div className="Maps">
         <ReactMapGL
+          mapboxAccessToken="pk.eyJ1IjoibmF5b2JhZXoiLCJhIjoiY2w4NHVxNHNoMDNoYzNvbHQ1MXE5cnd0ciJ9.grPjQHHspWWfEOx3aZWIRg"
           {...viewport}
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-          onViewportChange={(viewport) => {
-            setViewport(viewport);
+          initialViewState={(newView) => {
+            setViewport(newView);
           }}
+          style={{ width: 315, height: 165 }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
         />
       </div>
     </section>
