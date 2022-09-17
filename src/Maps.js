d7b14268c993c2db989ed958a970719ba691a050
NayoBaez <104393006+NayoBaez@users.noreply.github.com>
2022-09-16 20:53:02 -0400

    Updated Mapbox to move around

--- src/Maps.js
+++ src/Maps.js
@@ -3,20 +3,19 @@ import ReactMapGL from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 export default function Maps(props) {
-  const [viewport, setViewport] = useState({
+  let [viewport, setViewport] = useState({
     latitude: props.coordinates.lat,
     longitude: props.coordinates.lon,
     zoom: 10,
   });
+
   return (
     <section>
       <div className="Maps">
         <ReactMapGL
           mapboxAccessToken="pk.eyJ1IjoibmF5b2JhZXoiLCJhIjoiY2w4NHVxNHNoMDNoYzNvbHQ1MXE5cnd0ciJ9.grPjQHHspWWfEOx3aZWIRg"
           {...viewport}
-          initialViewState={(newView) => {
-            setViewport(newView);
-          }}
+          onMove={(evt) => setViewport(evt.viewport)}
           style={{ width: 315, height: 165 }}
           mapStyle="mapbox://styles/nayobaez/cl850n2k6002n14qkpfch5def"
         />
